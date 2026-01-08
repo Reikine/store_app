aa984e014e953c06bc69e172d5c497790403a57b
Reikine <otsukareikine@gmail.com>
2026-01-08 23:47:23 +0700

    feat: setup docker environment, fix db connection, and add go vendor

--- backend/config/db.go
+++ backend/config/db.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"marketplace-backend/models"
 	"os"
@@ -13,13 +14,20 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Tidak dapat menemukan file env")
-	}
+	godotenv.Load()
 	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+	}
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
+		log.Printf("Error detail: %v", err)
 		panic("Gagal tersambung ke Database")
 	}
 	database.AutoMigrate(&models.User{}, &models.Product{})
